23bf3c7c2fe1e63faae9a37c8c2a70964064cadf
abruckman <abruckman09@gmail.com>
2017-03-09 18:07:25 -0800

    got to complete map

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -2,16 +2,9 @@ class MapsController < ApplicationController
   def index
     p "<^>"
     respond_to do |format|
-      format.html 
+      format.html
       # format.json { render json: @resource }
     end
   end
 
-  def show
-    respond_to do |format|
-      format.html
-      format.js{}
-      # format.json { render json: @resource }
-    end
-  end
 end
